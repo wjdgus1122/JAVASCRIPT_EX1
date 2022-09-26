fb48c6d9a43bad1fdd32ef489c81358c5b45fcec
wjdgus1122 <106130435+wjdgus1122@users.noreply.github.com>
2022-09-26 22:07:56 +0900

    css change

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const num = document.getElementsByClassName(".clicknum");
 const conclick = (e) => {
   const numvalue = num[e].value;
   num[e].innerHTML = numvalue + 1;
+  console.log(numvalue);
 };
 
-context[e].addEventListener(click, conclick);
+context.addEventListener("click", conclick);
